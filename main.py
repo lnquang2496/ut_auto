6a4612a46e63afaa55f51247d880c8c7bd37b0bf
quangla <lnquang2496@gmail.com>
2019-12-16 00:15:33 +0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from os import system, path
 from gc import collect
 from pandas import read_csv
+from openpyxl import load_workbook
 
-class ws_handle():
+class ws_handle(object):
     def __init__(self, wb_dir, wb_name, ws_name):
-        from openpyxl import load_workbook
         wb = load_workbook(path.join(wb_dir, wb_name), data_only=True)
         self.ws = wb[ws_name]
         del wb
@@ -12,28 +12,26 @@ class ws_handle():
         #print(self.ws)
 
     def coor_find_cell(self, target, match_case:bool=False, find_all:bool=False):
-        return_value = list()
+        return_coor = []
         # If target is string type
         if isinstance(target, str):
             for rows in self.ws:
                 for cell in rows:
+                    print(cell.value)
                     target_found = False
                     if match_case:
                         target_found = target in cell.value
                     else:
                         target_found = target == cell.value
                     if target_found:
-                        del target_found
                         not_merged_cell = True
-                        for merged_cell in ws.merged_cells:
+                        for merged_cell in self.ws.merged_cells:
                             if (cell.coordinate in merged_cell):
-                                temp = (merged_cell.bounds[0],
-                                        merged_cell.bounds[1],
-                                        merged_cell.bounds[2],
-                                        merged_cell.bounds[3])
+                                temp = (merged_cell.bounds[0], merged_cell.bounds[1],
+                                        merged_cell.bounds[2], merged_cell.bounds[3])
                                 not_merged_cell = False
                                 if find_all:
-                                    return_value.append(temp)
+                                    return_coor.append(temp)
                                 else:
                                     return tuple(temp)
                         if not_merged_cell:
@@ -42,36 +40,33 @@ class ws_handle():
                                     cell.column,
                                     cell.row)
                             if find_all:
-                                return_value.append(temp)
+                                return_coor.append(temp)
                             else:
                                 return tuple(temp)
         # If target is list type, list format [column, row]
         elif isinstance(target, list):
             # list  = [col, row]
-            coor = ws.cell(column=target[0], row=target[1]).coordinate
+            coor = self.ws.cell(column=target[0], row=target[1]).coordinate
             not_merged_cell = True
-            for merged_cell in ws.merged_cells:
+            for merged_cell in self.ws.merged_cells:
                 if (coor in merged_cell):
-                    temp = {merged_cell.bounds[0],
-                            merged_cell.bounds[1],
-                            merged_cell.bounds[2],
-                            merged_cell.bounds[3]}
+                    temp = {merged_cell.bounds[0], merged_cell.bounds[1],
+                            merged_cell.bounds[2], merged_cell.bounds[3]}
                     not_merged_cell = False
                     return tuple(temp)
             if not_merged_cell:
-                temp = {target[0],
-                        target[1],
-                        target[0],
-                        target[1]}
+                temp = {target[0], target[1],
+                        target[0], target[1]}
                 return tuple(temp)
-        return return_value
+        return return_coor
+
+    def debug(self):
+        print(self.coor_find_cell("#", match_case=True))
+        print(self.coor_find_cell("Input factor", match_case=True))
 
 def main():
-    def extract_data(pd_data):
-        return pd_data["implement"], \
-        pd_data["workbook_dir"], \
-        pd_data["workbook_name"], \
-        pd_data["worksheet_name"]
+    def extract_data(x):
+        return x["implement"], x["workbook_dir"], x["workbook_name"], x["worksheet_name"]
 
     config_df = read_csv("config.csv", index_col="no")
     #print(config_df)
@@ -80,14 +75,14 @@ def main():
     while True:
         try:
             imp, wb_dir, wb_name, ws_name = extract_data(config_df.iloc[i])
-            #print(imp, wb_dir, wb_name, ws_name)
+            print(imp, wb_dir, wb_name, ws_name)
             if ("yes" == imp):
                 ws_obj = ws_handle(wb_dir, wb_name, ws_name)
-                #ws_obj.debug()
+                ws_obj.debug()
             i += 1
         except:
             break
 
 if __name__ == "__main__":
-    system("cls")
+    #system("cls")
     main()
