3798004ff2f347a68a119abcec94764eb5db5125
QuangLa <quang.la.gx@renesas.com>
2019-12-23 17:11:29 +0700

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class cell(object):
 		self.last_col = 0
 		self.last_row = 0
 		self.value = "None"
-		self.merged_cell = False
+		#self.merged_cell = False
 		self.find_cell(target, match_case)
 
 	def find_cell(self, target, match_case:bool=False):
@@ -22,11 +22,11 @@ class cell(object):
 				if cell.coordinate in merged_cell.coord:
 					temp = (merged_cell.bounds[0], merged_cell.bounds[1],
 							merged_cell.bounds[2], merged_cell.bounds[3])
-					self.merged_cell = True
+					#self.merged_cell = True
 					break
 			else:
 				temp = (cell.column, cell.row, cell.column, cell.row)
-				self.merged_cell = False
+				#self.merged_cell = False
 			self.first_col = temp[0]
 			self.first_row = temp[1]
 			self.last_col = temp[2]
@@ -43,6 +43,12 @@ class cell(object):
 		elif isinstance(target, list) or isinstance(target, tuple):
 			cell = self.ws.cell(column=target[0], row=target[1])
 			coor_merged_cell(cell)
+		elif target.value:
+			self.first_col = target.first_col
+			self.first_row = target.first_row
+			self.last_col = target.last_col
+			self.last_row = target.last_row
+			self.value = target.value
 		else:
 			print(f"target {target} is not str or list")
 		return False
@@ -66,14 +72,7 @@ class cell(object):
 		self.find_cell(temp)
 
 	def coor_print(self):
-		print("--------------")
-		print("first column : ", self.first_col)
-		print("first row    : ", self.first_row)
-		print("last column  : ", self.last_col)
-		print("last row     : ", self.last_row)
-		print("cell value   : ", self.value)
-		print("merged cell  : ", self.merged_cell)
-		print("--------------")
+		print(self.first_col, self.first_row, self.last_col, self.last_col, self.merged_cell, self.value, sep="\t")
 
 class testcase(object):
 	def __init__(self, ws, target):
@@ -94,18 +93,93 @@ class testcase(object):
 			coor.coor_shift_down()
 		self.last_tc_row = coor.first_row - 1
 
-	def testcase_print(self):
-		print("--------------")
-		print("tc column    : ", self.tc_col)
-		print("first tc row : ", self.first_tc_row)
-		print("last tc row  : ", self.last_tc_row)
-		print("total tc     : ", self.total_tc)
-		print("--------------")
-
 class inoutput_handler(object):
-	def __init__(ws, target, match_case:bool=False):
-		
-		pass
+	def __init__(self, ws):
+		self.ws = ws
+
+		def is_pointer(value):
+			with open("data/pointer.txt", "r") as f:
+				for l in f:
+					l = l.rstrip("\n")
+					if l != "*":
+						l += " "
+					if l in value:
+						return True
+				else:
+					return False
+
+		def is_structure(value):
+			with open("data/structure.txt", "r") as f:
+				for l in f:
+					l = l.rstrip("\n")
+					l += " "
+					if l in value:
+						return True
+						break
+				else:
+					return False
+		"""
+		def inoutput_search(obj, row_testcase):
+			obj_1 = cell(self.ws, obj)
+			obj_1.coor_shift_down()
+			while obj_1.last_col <= obj.last_col:
+				process = True
+				if is_pointer(obj_1.value):
+					pass
+				else:
+					if is_structure(obj_1.value):
+						inoutput_search(obj_1, row_testcase)
+						process = False
+				if process:
+					obj_1.coor_print()
+					print("\t\t\t\tvalue:\t", str(self.ws.cell(row_testcase, obj_1.first_col).value), sep="")
+				obj_1.coor_shift_right()
+			else:
+				del obj_1
+
+		obj_testcase = testcase(self.ws, "#")
+		obj_input = cell(self.ws, "Input factor")
+		for r in range(obj_testcase.first_tc_row, obj_testcase.last_tc_row + 1):
+			inoutput_search(obj_input, r)
+		obj_output = cell(self.ws, "Output element")
+		for r in range(obj_testcase.first_tc_row, obj_testcase.last_tc_row + 1):
+			inoutput_search(obj_output, r)
+		"""
+		def input_search(obj, row):
+			def structure_search(obj, row):
+				obj_temp = cell(self.ws, obj)
+				obj_temp.coor_shift_down()
+				while obj_temp.last_col <= obj.last_col:
+					if is_pointer(obj_temp.value):
+						pass
+					else:
+						if is_structure(obj_temp.value):
+							structure_search(obj_temp, row)
+					obj_temp.coor_shift_right()
+				else:
+					del obj_temp
+
+			obj_temp = cell(self.ws, obj)
+			obj_temp.coor_shift_down()
+			while obj_temp.last_col <= obj.last_col:
+				if "[a]" in obj_temp.value:
+					if is_pointer(obj_temp.value):
+						pass
+					else:
+						if is_structure(obj_temp.value):
+							structure_search(obj_temp, row)
+				elif "[rt]" in obj_temp.value:
+					pass
+				elif "[f]" in obj_temp.value:
+					pass
+				obj_temp.coor_shift_right()
+			else:
+				del obj_temp
+
+		obj_testcase = testcase(self.ws, "#")
+		obj_input = cell(self.ws, "Input factor")
+		for r in range(obj_testcase.first_tc_row, obj_testcase.last_tc_row + 1):
+			input_search(obj_input, r)
 
 def main():
 	def ws_get(wb_dir, wb_name, ws_name):
@@ -114,16 +188,14 @@ def main():
 	def extract_data(x):
 		return x["implement"], x["workbook_dir"], x["workbook_name"], x["worksheet_name"]
 
-	config_df = read_csv("config.csv", index_col="no")
+	config_df = read_csv("data/config.csv", index_col="no")
 
 	for i in range(config_df._AXIS_LEN):
 		imp, wb_dir, wb_name, ws_name = extract_data(config_df.iloc[i])
-		print(imp, wb_dir, wb_name, ws_name)
+		print(imp, wb_dir, wb_name, ws_name, sep="\t")
 		if ("yes" == imp):
 			ws = ws_get(wb_dir, wb_name, ws_name)
-			coor_temp = cell(ws, "Input factor")
-			coor_temp.coor_print()
-			tc_obj = testcase(ws, "#")
+			obj = inoutput_handler(ws)
 
 if __name__ == "__main__":
 	start = time()
